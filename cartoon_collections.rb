f52f78384eebe70cfb31e1fa7f90af529fa865d3
Eliot Howes <ejhowes@gmail.com>
2019-06-10 21:24:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,9 @@ def roll_call_dwarves(dwarves)# code an argument here
   dwarves.each_with_index {|dwarf, index| puts "#{index + 1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(veggies)# code an argument here
   # Your code here
+  veggies.collect {|veg| "#{veg.capitalize}!"}
 end
 
 def long_planeteer_calls# code an argument here
