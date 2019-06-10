8d81db96aa0936160e1a02d66aa3dc3fd9c2ac6c
Eliot Howes <ejhowes@gmail.com>
2019-06-10 22:01:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,6 @@ def find_the_cheese(contains_cheddar)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
   contains_cheddar.any? do |cheese|
     binding.pry
-    cheese_types.include?(cheese) ? cheese : nil
+    cheese_types.include?(cheese) ? puts cheese : nil
   end
 end
